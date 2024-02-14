6c568a9c25127e1d4c5c9ec5978b92eac0c6d837
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2024-02-13 21:07:21 -0800

    first commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
